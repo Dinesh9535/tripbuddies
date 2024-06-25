c0a719fef530566053da8beae6402a760efa55ab
Pratyasha Singhdeo <pratyasha.singhdeo@valtech.com>
2024-06-25 18:24:03 +0530

    BE: changes in payment wrt staipe

--- spring-backend/stripe-service/src/main/java/com/tripbuddies/StripeServiceApplication.java
+++ spring-backend/stripe-service/src/main/java/com/tripbuddies/StripeServiceApplication.java
@@ -4,11 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
-@EnableDiscoveryClient
 @ConfigurationPropertiesScan
 public class StripeServiceApplication {
 
--- spring-backend/stripe-service/src/main/java/com/tripbuddies/controller/PaymentGatewayController.java
+++ spring-backend/stripe-service/src/main/java/com/tripbuddies/controller/PaymentGatewayController.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin("*")
+@CrossOrigin(origins = { "*" })
 @RequestMapping("/stripe-api/payment")
 public class PaymentGatewayController {
 
